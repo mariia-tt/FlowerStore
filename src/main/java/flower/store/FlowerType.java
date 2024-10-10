49da92d1919a9f02ed8d120838e2e5cea6b2a6a8
Mariia <masha.tzimbal@gmail.com>
2024-10-10 15:08:37 +0300

    Add space

--- src/main/java/flower/store/FlowerType.java
+++ src/main/java/flower/store/FlowerType.java
@@ -5,19 +5,22 @@ public enum FlowerType {
 }
 
 class CHAMOMILE extends Flower{
-    public CHAMOMILE(double sepalLength, FlowerColor color, double price, FlowerType flowerTypee){
+    public CHAMOMILE(double sepalLength, FlowerColor color,
+     double price) {
         super(sepalLength, color, price, FlowerType.CHAMOMILE);
     }
 }
 
 class ROSE extends Flower{
-    public ROSE(double sepalLength, FlowerColor color, double price, FlowerType flowerTypee){
-        super(sepalLength, color, price, FlowerType.CHAMOMILE);
+    public ROSE(double sepalLength, FlowerColor color,
+     double price) {
+        super(sepalLength, color, price, FlowerType.ROSE);
     }
 }
 
 class TULIP extends Flower{
-    public TULIP(double sepalLength, FlowerColor color, double price, FlowerType flowerTypee){
-        super(sepalLength, color, price, FlowerType.CHAMOMILE);
+    public TULIP(double sepalLength, FlowerColor color,
+     double price) {
+        super(sepalLength, color, price, FlowerType.TULIP);
     }
 }
