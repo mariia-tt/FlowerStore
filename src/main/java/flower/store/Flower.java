f42b6e0e71545c7ff2a4708ca84b6f7655d96f03
Mariia <masha.tzimbal@gmail.com>
2024-10-13 14:41:20 +0300

    Add FlowerFactoty

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -25,25 +25,3 @@ public class Flower {
         return color.toString();
     }
 }
-
-class CHAMOMILE extends Flower{
-    public CHAMOMILE(double sepalLength, FlowerColor color,
-     double price) {
-        super(sepalLength, color, price, FlowerType.CHAMOMILE);
-    }
-}
-
-class ROSE extends Flower{
-    public ROSE(double sepalLength, FlowerColor color,
-     double price) {
-        super(sepalLength, color, price, FlowerType.ROSE);
-    }
-}
-
-class TULIP extends Flower{
-    public TULIP(double sepalLength, FlowerColor color,
-     double price) {
-        super(sepalLength, color, price, FlowerType.TULIP);
-    }
-}
-
--- src/main/java/flower/store/FlowerType.java
+++ src/main/java/flower/store/FlowerType.java
@@ -4,23 +4,3 @@ public enum FlowerType {
     CHAMOMILE, ROSE, TULIP;
 }
 
-class CHAMOMILE extends Flower{
-    public CHAMOMILE(double sepalLength, FlowerColor color,
-     double price) {
-        super(sepalLength, color, price, FlowerType.CHAMOMILE);
-    }
-}
-
-class ROSE extends Flower{
-    public ROSE(double sepalLength, FlowerColor color,
-     double price) {
-        super(sepalLength, color, price, FlowerType.ROSE);
-    }
-}
-
-class TULIP extends Flower{
-    public TULIP(double sepalLength, FlowerColor color,
-     double price) {
-        super(sepalLength, color, price, FlowerType.TULIP);
-    }
-}
