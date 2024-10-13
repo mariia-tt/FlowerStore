1c5e7f0001243cd57f24dca37c6004659b5d43f1
Mariia <masha.tzimbal@gmail.com>
2024-10-13 14:29:32 +0300

    Change Checkstyle

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -26,3 +26,24 @@ public class Flower {
     }
 }
 
+class CHAMOMILE extends Flower{
+    public CHAMOMILE(double sepalLength, FlowerColor color,
+     double price) {
+        super(sepalLength, color, price, FlowerType.CHAMOMILE);
+    }
+}
+
+class ROSE extends Flower{
+    public ROSE(double sepalLength, FlowerColor color,
+     double price) {
+        super(sepalLength, color, price, FlowerType.ROSE);
+    }
+}
+
+class TULIP extends Flower{
+    public TULIP(double sepalLength, FlowerColor color,
+     double price) {
+        super(sepalLength, color, price, FlowerType.TULIP);
+    }
+}
+
